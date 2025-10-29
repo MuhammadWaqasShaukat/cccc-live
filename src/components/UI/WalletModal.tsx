2286b0eeebd5a166ae611bff2b527ef7a8bba37a
Raja Waqas <rajawaqas912@gmail.com>
2025-10-29 10:56:44 +0500

    chore: new deployment 29 oct:1

--- src/components/UI/WalletModal.tsx
+++ src/components/UI/WalletModal.tsx
@@ -65,7 +65,7 @@ const WalletModal = memo(() => {
             <button
               key={`${wallet.adapter.name}-${i}`}
               onClick={() => handleConnect(wallet.adapter.name)}
-              className="flex flex-row justify-between items-center gap-3 w-full pl-5 pr-4 py-3 rounded-xl transition  bg-white/40 hover:bg-white"
+              className="flex flex-row justify-between items-center gap-3 w-full pl-5 pr-4 py-4 max-h-[107px] max-w-[310px] rounded-xl transition  bg-white/40 hover:bg-white"
             >
               <span className="text-2xl font-medium uppercase font-patrick-hand-sc">
                 {wallet.adapter.name}
@@ -85,21 +85,15 @@ const WalletModal = memo(() => {
 
   return (
     <Modal onBackgroundClick={() => setVisible(false)}>
-      <div className="bg-sm-mint-section-book bg-cover bg-no-repeat h-max p-3 flex flex-col justify-center items-center md:gap-6 gap-4 rounded-2xl w-[350px] md:w-[400px]">
-        {/* <div className="bg-white-listing-open bg-contain w-full md:h-48 h-40 bg-center bg-no-repeat grid place-content-center">
-          <h2 className="text-3xl uppercase text-black font-patrick-hand-sc text-center leading-10">
-            Connect Wallet
-          </h2>
-        </div> */}
-
-        <h2 className="text-3xl uppercase text-black font-patrick-hand-sc text-center leading-10">
+      <div className="bg-sm-mint-section-book bg-cover bg-no-repeat h-max p-2 flex flex-col justify-center items-center md:gap-6 gap-4 rounded-2xl w-[350px] md:w-[355px]">
+        <h2 className="text-[28px]  uppercase text-black font-patrick-hand-sc text-center leading-[52px]">
           Connect Wallet
         </h2>
-        <div className="space-y-4 px-4 flex flex-col w-full min-h-40 max-h-96 overflow-scroll filter-list">
+        <div className="space-y-4 px-2.5  flex flex-col w-full min-h-40 max-h-96 overflow-scroll filter-list">
           {walletButtons}
         </div>
 
-        <div className="bg-white-listing-connect bg-contain bg-center bg-no-repeat h-28 w-full p-6 mx-auto"></div>
+        <div className="bg-white-listing-connect bg-contain bg-center bg-no-repeat h-28 w-[226px] p-6 mx-auto"></div>
         <div className="w-full text-center">
           <span className="block underline font-patrick-hand-sc text-base px-4">
             &nbsp; &nbsp; &nbsp; &nbsp; Terms And Conditions &nbsp; &nbsp;
